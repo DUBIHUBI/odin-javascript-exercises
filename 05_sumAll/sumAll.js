7eb86f4c9c4284eb8579a80873dfe390643580b8
Hubert Yu <hubert12161999@gmail.com>
2022-12-04 23:52:26 +0000

    Finish sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (smallNum, largeNum) {
+  if (!Number.isInteger(smallNum) || !Number.isInteger(largeNum)) {
+    return 'ERROR';
+  }
+  if (smallNum < 0 || largeNum < 0) return 'ERROR';
+  let sum = 0;
+  if (smallNum > largeNum) {
+    let temp = smallNum;
+    smallNum = largeNum;
+    largeNum = temp;
+  }
+  for (let i = smallNum; i <= largeNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
