ddc2369a9bb4a56e4ee0aa1adf7e05a76bfe50e9
Hubert Yu <hubert12161999@gmail.com>
2022-12-04 21:14:26 +0000

    Delete a line in the second solution of removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,6 @@
 const removeFromArray = function (array, ...remove) {
   // for (const a of remove) {
   //   array = array.filter((element) => element !== a);
-  //   array.includes(a);
   // }
   // return array;
   return array.filter((element) => !remove.includes(element));
