1366af440d7b3a09a6a4ca9eceaa86803b51b3c8
Hubert Yu <hubert12161999@gmail.com>
2022-12-04 21:08:46 +0000

    Finish removeFromArray function with 2 solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...remove) {
+  // for (const a of remove) {
+  //   array = array.filter((element) => element !== a);
+  //   array.includes(a);
+  // }
+  // return array;
+  return array.filter((element) => !remove.includes(element));
 };
 
 // Do not edit below this line
