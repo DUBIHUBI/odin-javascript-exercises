37ac08167d8aaa519101d86869b27001d1f824b2
Hubert Yu <hubert12161999@gmail.com>
2022-12-14 02:30:55 +0000

    Finish all 6 maths functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNum, secondNum) {
+  return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function (arrayOfNums) {
+  return arrayOfNums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arrayOfNums) {
+  if (arrayOfNums.length) {
+    return arrayOfNums.reduce((total, num) => total * num, 1);
+  }
+  return 0;
 };
 
-const power = function() {
-	
+const power = function (firstNum, secondNum) {
+  return firstNum ** secondNum;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
