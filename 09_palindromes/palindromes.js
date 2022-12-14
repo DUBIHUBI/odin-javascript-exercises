0b05fc6e18b63b039ebc1b3483fe58185877ee5d
Hubert Yu <hubert12161999@gmail.com>
2022-12-14 20:21:17 +0000

    Finish palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let onlyLetters = string.toLowerCase().replace(/[^a-z]/g, '');
+  let reversedString = onlyLetters.split('').reverse().join('');
+  return reversedString === onlyLetters;
 };
 
 // Do not edit below this line
