2c6f1b771d139054ab34f82c5cf253d24b2f1ed6
Hubert Yu <hubert12161999@gmail.com>
2022-12-14 21:16:47 +0000

    Finish fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return 'OOPS';
+  if (num === 0) return 0;
+  let num1 = 1;
+  let num2 = 1;
+  let temp = 0;
+  for (let i = 2; i < num; i++) {
+    temp = num2;
+    num2 += num1;
+    num1 = temp;
+  }
+  return num2;
 };
 
 // Do not edit below this line
