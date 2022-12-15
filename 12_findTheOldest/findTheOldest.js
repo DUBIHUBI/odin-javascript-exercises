a5d14b715699761c05a1239ab3cce3c10ab80e8e
Hubert Yu <hubert12161999@gmail.com>
2022-12-15 01:21:27 +0000

    Finish findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  let currentYear = new Date().getFullYear();
+  let oldestPerson = array.reduce((oldest, nextPerson) => {
+    let oldestDeath =
+      'yearOfDeath' in oldest ? oldest.yearOfDeath : currentYear;
+    let oldestAge = oldestDeath - oldest.yearOfBirth;
+    let nextPersonDeath =
+      'yearOfDeath' in nextPerson ? nextPerson.yearOfDeath : currentYear;
+    let nextPersonAge = nextPersonDeath - nextPerson.yearOfBirth;
+    return nextPersonAge > oldestAge ? nextPerson : oldest;
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
